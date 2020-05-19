de007ccf1fdcd928ea2925c0378ac49534638cbc
aakarshy <aakarshyadav.iitkgp@gmail.com>
2020-05-19 19:04:08 +0530

    home jumbotron edit

--- src/core/Home.js
+++ src/core/Home.js
@@ -6,13 +6,13 @@ import Logo from "../images/logo.png"
 const Home = () => (
 	<div>
 		<div className="jumbotron row">
-		<div className="col-md-2">
+		<div className="col-md-4">
 			<span className="row"><img src={Logo} height="100px" width="auto" />
 			<div className="ml-3"><h3><strong> ALTER 2.2 </strong></h3>
 			<hr /><p className="lead text-center">FORUM </p></div>
 			</span>
 		</div>
-		<div className="col-md-10 text-center">
+		<div className="col-md-8 text-center">
 			<h1> Work in progress.... </h1>
 		</div>
 		</div>
