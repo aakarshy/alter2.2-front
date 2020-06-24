c8d7cc71c062780eef4f5e7bc1719f15da2f469f
aakarshy <aakarshyadav.iitkgp@gmail.com>
2020-06-24 21:38:28 +0530

    Added Privacy Policy

--- src/core/Home.js
+++ src/core/Home.js
@@ -37,10 +37,7 @@ const Home = () => (
 			<Posts />
 		</div>
 
-  <div class="footer"><a href="https://sites.google.com/view/privacy-policy-alter/home">
-  	Privacy Policy
-  	</a>
-  	</div>
+<a href="https://www.iubenda.com/privacy-policy/48668821" class="iubenda-white iubenda-embed" title="Privacy Policy ">Privacy Policy</a><script type="text/javascript">(function (w,d) {var loader = function () {var s = d.createElement("script"), tag = d.getElementsByTagName("script")[0]; s.src="https://cdn.iubenda.com/iubenda.js"; tag.parentNode.insertBefore(s,tag);}; if(w.addEventListener){w.addEventListener("load", loader, false);}else if(w.attachEvent){w.attachEvent("onload", loader);}else{w.onload = loader;}})(window, document);</script>
   </div>
 	</div>
 
